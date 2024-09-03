5dab05cb65b0d7d0f76e0c206c61890a4c4c41af
Yotam Alon <yotamalon@gmail.com>
2024-09-03 21:43:03 +0300

    Added output channel

--- src/extension.ts
+++ src/extension.ts
@@ -3,38 +3,78 @@
 import * as vscode from 'vscode';
 import path from 'path';
 import { PythonExtension } from '@vscode/python-extension';
-
+import * as util from 'util';
 import {
-  LanguageClient,
-  LanguageClientOptions,
-  ServerOptions,
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
 } from "vscode-languageclient/node";
 
 const lspConfigKey = "pylsp";
 let client: LanguageClient;
 
 function getLanguageClient(
-    bundleDir: string,
-  command: string,
-  args: string[],
-  documentSelector: string[]
+	bundleDir: string,
+	command: string,
+	args: string[],
+	documentSelector: string[],
+	outputChannel: vscode.LogOutputChannel,
 ): LanguageClient {
-  const serverOptions: ServerOptions = {
-    command: command,
-    args: args,
-    options: {cwd: bundleDir}
-  };
-  const clientOptions: LanguageClientOptions = {
-    documentSelector: documentSelector,
-    synchronize: {
-      configurationSection: lspConfigKey,
-    },
-  };
-  return new LanguageClient(command, serverOptions, clientOptions);
+	const serverOptions: ServerOptions = {
+		command: command,
+		args: args,
+		options: { cwd: bundleDir }
+	};
+	const clientOptions: LanguageClientOptions = {
+		documentSelector: documentSelector,
+		synchronize: {
+			configurationSection: lspConfigKey,
+		},
+		outputChannel: outputChannel
+	};
+	return new LanguageClient(command, serverOptions, clientOptions);
+}
+
+type Arguments = unknown[];
+class OutputChannelLogger {
+    constructor(private readonly channel: vscode.LogOutputChannel) {}
+
+    public traceLog(...data: Arguments): void {
+        this.channel.appendLine(util.format(...data));
+    }
+
+    public traceError(...data: Arguments): void {
+        this.channel.error(util.format(...data));
+    }
+
+    public traceWarn(...data: Arguments): void {
+        this.channel.warn(util.format(...data));
+    }
+
+    public traceInfo(...data: Arguments): void {
+        this.channel.info(util.format(...data));
+    }
+
+    public traceVerbose(...data: Arguments): void {
+        this.channel.debug(util.format(...data));
+    }
+}
+
+let channel: OutputChannelLogger | undefined;
+export function registerLogger(outputChannel: vscode.LogOutputChannel): vscode.Disposable {
+    channel = new OutputChannelLogger(outputChannel);
+    return {
+        dispose: () => {
+            channel = undefined;
+        },
+    };
 }
 
 export async function activate(context: vscode.ExtensionContext) {
+	let outputChannel = vscode.window.createOutputChannel("Python Refactoring", { log: true });
+	context.subscriptions.push(outputChannel, registerLogger(outputChannel));
 	const pythonRootDir = path.join(__dirname, '..', 'bundled');
+	
 	const pythonApi: PythonExtension = await PythonExtension.api();
 	const environmentPath = pythonApi.environments.getActiveEnvironmentPath();
 
@@ -45,51 +85,25 @@ export async function activate(context: vscode.ExtensionContext) {
 	}
 	const environment = resolvedEnvironment;
 
-	let projectDir: string;
-	const workspacePaths = vscode.workspace.workspaceFolders?.map(folder => folder.uri.path);
-	if (!workspacePaths || workspacePaths?.length === 0) {
-		vscode.window.showInformationMessage('No project selected');
-		return null;
-	} else if (workspacePaths.length > 1) {
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			vscode.window.showInformationMessage('No editor is open');
-			return null;
-		}
+	const getClient = () => getLanguageClient(pythonRootDir, environment.path, ["-m", "pylsp", "-vv"], ["python"], outputChannel);
 
-		const document = editor.document;
-		if (!document) {
-			vscode.window.showInformationMessage('No file selected');
-			return null;
-		}
+	client = getClient();
 
-		const chosenProjectDir = workspacePaths.find(path => document.fileName.includes(path));
-		if (!chosenProjectDir) {
-			vscode.window.showInformationMessage('No project contains the open file');
-			return null;
-		}
-		projectDir = chosenProjectDir;
-	} else {
-		projectDir = workspacePaths[0];
-	}
-  const getClient = () => getLanguageClient(pythonRootDir, environment.path, ["-m", "pylsp", "-vv"], ["python"]);
+	context.subscriptions.push(client.start());
 
-  client = getClient();
-  context.subscriptions.push(client.start());
-
-  context.subscriptions.push(
-    vscode.commands.registerCommand(`${lspConfigKey}.restartServer`, async () => {
-      await killServer();
-      client = getClient();
-      client.start();
-    })
-  );
+	context.subscriptions.push(
+		vscode.commands.registerCommand(`${lspConfigKey}.restartServer`, async () => {
+			await killServer();
+			client = getClient();
+			client.start();
+		})
+	);
 }
 
 export function deactivate(): Thenable<void> | undefined {
-  return killServer();
+	return killServer();
 }
 
 async function killServer(): Promise<void> {
-  await client.stop();
+	await client.stop();
 }
